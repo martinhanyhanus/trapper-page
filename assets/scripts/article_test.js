ebda832d01a7eaacf46e27598f7ad7963da09f6e
Martin Hanuš <hanusm13@fit.cvut.cz>
2024-02-02 22:33:34 +0100

    article css

--- assets/scripts/article_test.js
+++ assets/scripts/article_test.js
@@ -26,9 +26,12 @@ class Article {
 		const wrapper = document.createElement("div");
 		wrapper.className = "article";
 
+		const article = document.createElement("div");
+		article.classList.add("article-text");
+
 		const title = document.createElement("h2");
 		title.innerHTML = this.title;
-		wrapper.appendChild(title);
+		article.appendChild(title);
 
 		const metadata = document.createElement("p");
 		metadata.className = "metadata";
@@ -36,11 +39,13 @@ class Article {
 		if (this.createdTimestamp + 15 * 60000 < this.lastEditedTimestamp) {
 			metadata.innerHTML += ", Editováno: " + new Date(this.lastEditedTimestamp).toLocaleString();
 		}
-		wrapper.appendChild(metadata);
+		article.appendChild(metadata);
 
 		const text = document.createElement("p");
 		text.innerHTML = this.text;
-		wrapper.appendChild(text);
+		article.appendChild(text);
+
+		wrapper.appendChild(article);
 
 		if(this.image != "") {
 			const image = document.createElement("img");
@@ -59,8 +64,6 @@ let articles = [];
 articles.push(new Article("Kremže", "Tandem", "Křemže byla. Trvala pár dní a něco se tam dělalo.", "http://tc.westerners.cz/wp-content/tc_logo.gif"));
 articles.push(new Article("Halloween", "Edík", "Halloween byl, byly nějaké masky a dlouhá valná hromada.", "", 10));
 articles.push(new Article("Díkuvzdání", "Edík", "Díkuvzdání bylo, byl sníh a hodně jídla.", "https://www.beavercity.cz/images/DSC_6904_1.jpg", 20));
-
-
 
 articles.forEach(function (article) {
 	document.getElementById("articles").appendChild(article.articleElement);
